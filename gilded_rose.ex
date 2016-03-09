a55523e7f3274959283fbe73e517d63fee0c06a4
Aaron Tinio <aptinio@gmail.com>
2016-03-09 09:39:55 +0800

    Remove original function body and treat items as normal items by default

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -52,55 +52,10 @@ defmodule GildedRose do
   def update_quality(items) do
     Enum.map(items, fn(item) ->
       case item.name do
-        "normal" -> update_normal_item(item)
         "Aged Brie" -> update_aged_brie(item)
         "Sulfuras, Hand of Ragnaros" -> update_sulfuras(item)
         "Backstage passes to a TAFKAL80ETC concert" -> update_backstage_passes(item)
-        _ ->
-          if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" do
-            if item.quality > 0 do
-              if item.name != "Sulfuras, Hand of Ragnaros" do
-                item = %{item | quality: item.quality - 1}
-              end
-            end
-          else
-            if item.quality < 50 do
-              item = %{item | quality: item.quality + 1}
-              if item.name == "Backstage passes to a TAFKAL80ETC concert" do
-                if item.sell_in < 11 do
-                  if item.quality < 50 do
-                    item = %{item | quality: item.quality + 1}
-                  end
-                end
-                if item.sell_in < 6 do
-                  if item.quality < 50 do
-                    item = %{item | quality: item.quality + 1}
-                  end
-                end
-              end
-            end
-          end
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            item = %{item | sell_in: item.sell_in - 1}
-          end
-          if item.sell_in < 0 do
-            if item.name != "Aged Brie" do
-              if item.name != "Backstage passes to a TAFKAL80ETC concert" do
-                if item.quality > 0 do
-                  if item.name != "Sulfuras, Hand of Ragnaros" do
-                    item = %{item | quality: item.quality - 1}
-                  end
-                end
-              else
-                item = %{item | quality: item.quality - item.quality}
-              end
-            else
-              if item.quality < 50 do
-                item = %{item | quality: item.quality + 1}
-              end
-            end
-          end
-          item
+        _ -> update_normal_item(item)
       end
     end)
   end
