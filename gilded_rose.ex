b15d73c124a33c5dd271f8c81a905db5571888b0
Sam Livingston-Gray <geeksam@gmail.com>
2017-09-10 10:55:39 -0700

    Extract function

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -8,51 +8,53 @@ defmodule GildedRose do
   # Your job is to now make it beautiful.
 
   def update_quality(items) do
-    Enum.map(items, fn(item) ->
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" do
-        if item.quality > 0 do
-          if item.name != "Sulfuras, Hand of Ragnaros" do
-            item = %{item | quality: item.quality - 1}
+    Enum.map( items, &(update_item(&1) ) )
+  end
+
+  def update_item(item) do
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" do
+      if item.quality > 0 do
+        if item.name != "Sulfuras, Hand of Ragnaros" do
+          item = %{item | quality: item.quality - 1}
+        end
+      end
+    else
+      if item.quality < 50 do
+        item = %{item | quality: item.quality + 1}
+        if item.name == "Backstage passes to a TAFKAL80ETC concert" do
+          if item.sell_in < 11 do
+            if item.quality < 50 do
+              item = %{item | quality: item.quality + 1}
+            end
           end
+          if item.sell_in < 6 do
+            if item.quality < 50 do
+              item = %{item | quality: item.quality + 1}
+            end
+          end
+        end
+      end
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros" do
+      item = %{item | sell_in: item.sell_in - 1}
+    end
+    if item.sell_in < 0 do
+      if item.name != "Aged Brie" do
+        if item.name != "Backstage passes to a TAFKAL80ETC concert" do
+          if item.quality > 0 do
+            if item.name != "Sulfuras, Hand of Ragnaros" do
+              item = %{item | quality: item.quality - 1}
+            end
+          end
+        else
+          item = %{item | quality: item.quality - item.quality}
         end
       else
         if item.quality < 50 do
           item = %{item | quality: item.quality + 1}
-          if item.name == "Backstage passes to a TAFKAL80ETC concert" do
-            if item.sell_in < 11 do
-              if item.quality < 50 do
-                item = %{item | quality: item.quality + 1}
-              end
-            end
-            if item.sell_in < 6 do
-              if item.quality < 50 do
-                item = %{item | quality: item.quality + 1}
-              end
-            end
-          end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros" do
-        item = %{item | sell_in: item.sell_in - 1}
-      end
-      if item.sell_in < 0 do
-        if item.name != "Aged Brie" do
-          if item.name != "Backstage passes to a TAFKAL80ETC concert" do
-            if item.quality > 0 do
-              if item.name != "Sulfuras, Hand of Ragnaros" do
-                item = %{item | quality: item.quality - 1}
-              end
-            end
-          else
-            item = %{item | quality: item.quality - item.quality}
-          end
-        else
-          if item.quality < 50 do
-            item = %{item | quality: item.quality + 1}
-          end
-        end
-      end
-      item
-    end)
+    end
+    item
   end
 end
