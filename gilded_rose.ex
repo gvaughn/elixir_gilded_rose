7490e075ac2a069c40f124265c3b32e8295849c7
Sam Livingston-Gray <geeksam@gmail.com>
2017-09-10 10:57:27 -0700

    Extract function for aged brie

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -11,8 +11,33 @@ defmodule GildedRose do
     Enum.map( items, &(update_item(&1) ) )
   end
 
+  def update_item( item = %Item{ name: "Aged Brie" } ) do
+    if item.quality < 50 do
+      item = %{item | quality: item.quality + 1}
+      if item.name == "Backstage passes to a TAFKAL80ETC concert" do
+        if item.sell_in < 11 do
+          if item.quality < 50 do
+            item = %{item | quality: item.quality + 1}
+          end
+        end
+        if item.sell_in < 6 do
+          if item.quality < 50 do
+            item = %{item | quality: item.quality + 1}
+          end
+        end
+      end
+    end
+    item = %{item | sell_in: item.sell_in - 1}
+    if item.sell_in < 0 do
+      if item.quality < 50 do
+        item = %{item | quality: item.quality + 1}
+      end
+    end
+    item
+  end
+
   def update_item(item) do
-    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" do
+    if item.name != "Backstage passes to a TAFKAL80ETC concert" do
       if item.quality > 0 do
         if item.name != "Sulfuras, Hand of Ragnaros" do
           item = %{item | quality: item.quality - 1}
@@ -39,20 +64,14 @@ defmodule GildedRose do
       item = %{item | sell_in: item.sell_in - 1}
     end
     if item.sell_in < 0 do
-      if item.name != "Aged Brie" do
-        if item.name != "Backstage passes to a TAFKAL80ETC concert" do
-          if item.quality > 0 do
-            if item.name != "Sulfuras, Hand of Ragnaros" do
-              item = %{item | quality: item.quality - 1}
-            end
+      if item.name != "Backstage passes to a TAFKAL80ETC concert" do
+        if item.quality > 0 do
+          if item.name != "Sulfuras, Hand of Ragnaros" do
+            item = %{item | quality: item.quality - 1}
           end
-        else
-          item = %{item | quality: item.quality - item.quality}
         end
       else
-        if item.quality < 50 do
-          item = %{item | quality: item.quality + 1}
-        end
+        item = %{item | quality: item.quality - item.quality}
       end
     end
     item
