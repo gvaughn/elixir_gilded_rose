6a768e3992b90812afe86aaf6e33b11872e22c05
Sam Livingston-Gray <geeksam@gmail.com>
2017-09-10 16:59:40 -0700

    Only update quality once, after aging. Rename functions.

--- gilded_rose.ex
+++ gilded_rose.ex
@@ -7,37 +7,32 @@ defmodule GildedRose do
 
   defp update_item(item) do
     item
-    |> update_quality_before_aging
-    |> age_item
-    |> update_quality_after_aging
+    |> update_item_sell_in
+    |> update_item_quality
     |> enforce_quality_constraints
-  end
-
-  defp update_quality_before_aging( item = %Item{} ) do
-    item = %{item | quality: item.quality + quality_modifier(item) }
   end
 
   defp quality_modifier( item = %Item{ name: "Sulfuras, Hand of Ragnaros" } ),                                                    do: 0
   defp quality_modifier( item = %Item{ name: "Aged Brie" } ),                                                                     do: 1
   defp quality_modifier( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in } ) when sell_in <  0, do: -item.quality
-  defp quality_modifier( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in } ) when sell_in <  6, do: 3
-  defp quality_modifier( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in } ) when sell_in < 11, do: 2
+  defp quality_modifier( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in } ) when sell_in <  5, do: 3
+  defp quality_modifier( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert", sell_in: sell_in } ) when sell_in < 10, do: 2
   defp quality_modifier( item = %Item{ name: "Backstage passes to a TAFKAL80ETC concert" } ),                                     do: 1
   defp quality_modifier( item = %Item{ name: "Conjured Mana Cake" } ),                                                            do: -2
   defp quality_modifier( item = %Item{} ),                                                                                        do: -1
 
-  defp age_item( item = %Item{ name: "Sulfuras, Hand of Ragnaros" } ) do
+  defp update_item_sell_in( item = %Item{ name: "Sulfuras, Hand of Ragnaros" } ) do
     item
   end
-  defp age_item( item = %Item{ sell_in: sell_in } ) do
+  defp update_item_sell_in( item = %Item{ sell_in: sell_in } ) do
     %{item | sell_in: item.sell_in - 1}
   end
 
-  defp update_quality_after_aging( item = %Item{ sell_in: sell_in } ) when sell_in < 0 do
-    %{item | quality: item.quality + quality_modifier(item) }
+  defp update_item_quality( item = %Item{ sell_in: sell_in } ) when sell_in < 0 do
+    %{item | quality: item.quality + quality_modifier(item) * 2 }
   end
-  defp update_quality_after_aging( item = %Item{} ) do
-    item
+  defp update_item_quality( item = %Item{} ) do
+    %{item | quality: item.quality + quality_modifier(item) }
   end
 
   defp enforce_quality_constraints(item = %Item{ name: "Sulfuras, Hand of Ragnaros" } ), do: %{ item | quality: 80 }
